bb9c479c60431a8364860aa03f18e76665692a5b
Leo Xuzhang Lin <me@leoxlin.com>
2026-07-07 22:40:03 -0400

    cttw: Wired both `cttw` and `cttw tui` through one clean alternate-scree

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,26 +1,13 @@
 package cli
 
-import (
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/llin/cttw/internal/config"
-	"github.com/llin/cttw/internal/tui"
-	"github.com/spf13/cobra"
-)
+import "github.com/spf13/cobra"
 
 func Run() error {
 	root := &cobra.Command{
 		Use:   "cttw",
 		Short: "Claudivicus Take The Wheel",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			cfg, err := config.Load(os.Getenv("CTTW_CONFIG"), nil)
-			if err != nil {
-				return err
-			}
-			p := tea.NewProgram(tui.New(cfg.DaemonSocket))
-			_, err = p.Run()
-			return err
+			return runTUI()
 		},
 	}
 	root.AddCommand(daemonCmd(), problemCmd(), problemsCmd(), tuiCmd())
